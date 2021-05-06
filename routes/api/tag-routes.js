a8893a9d4ef684b5c4a1dc2495c9f932c7c37e11
Jose Jimenez <josejimenez826@gmail.com>
2021-05-06 16:32:47 -0700

    worked on routes and and started one adding data

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,34 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try{
+    // find all tags
+    // be sure to include its associated Product data
+    const tagdata = await Tag.findAll().catch((err) => res.json(err));
+    const tags = tagdata.map((tag) => tag.get({plain: true}))
+    
+    res.send(tags);
+    res.status(200) 
+  }catch (err) {
+    res.status(500).json(err);
+  }
 });
-
-router.get('/:id', (req, res) => {
+ 
+router.get('/:id', async (req, res) => { // find why it doesn't return corecct thing
+  try{
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  const tagdata = await Tag.findByPk(req.body.id);
+  if(!tagdata){
+    res.status(404).json({message: 'No tag with this id!'})
+    return;
+  }
+  const tag = tagdata.get({plain: true})
+  res.send(tag)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
